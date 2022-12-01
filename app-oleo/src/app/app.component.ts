902b9740bdc249e67e72bc2ecac097ea064afedc
Aldo Borghesi <aldoborghesi@gmail.com>
2022-12-01 10:07:37 -0300

    Atividade 08 - Criação do layout com componentes e uso do conceito de data-binding

--- app-oleo/src/app/app.component.ts
+++ app-oleo/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import * as M from 'materialize-css';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app-oleo';
+  username = 'Usuário logado: Aldo Borghesi';
+  public formMessage!:string;
+  public emailAddress!:string;
+
+  public getUsername() {
+    return this.username;
+  }
+
+  public onSubmitForm() {
+    return this.formMessage = 'Obrigado por se registar. Vamos enviar um e-mail de confirmação.';
+  }
 }
--- app-oleo/src/app/footer/footer.component.ts
+++ app-oleo/src/app/footer/footer.component.ts
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  year: number;
+  constructor() {
+    this.year = new Date().getFullYear();
 
-  constructor() { }
+   }
 
   ngOnInit(): void {
   }
